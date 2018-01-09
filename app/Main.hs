50d8b3b5317034bacb2e2ae68187dc61b41ff9b8
Ryota Kameoka <kameoka.ryota@gmail.com>
2018-01-10 03:00:38 +0900

    Enable to specify order via command line option

--- app/Main.hs
+++ app/Main.hs
@@ -2,38 +2,50 @@
 
 module Main where
 
-import Control.Monad (when)
-import Data.Bool (bool)
+import Control.Monad (unless)
 import Data.Monoid ((<>))
 import Data.List (isPrefixOf)
 import Options.Applicative
 import System.Environment (getArgs)
-import System.Exit (exitFailure)
+import System.Exit (die, exitFailure)
 import Web.MarkovBot (getTwInfoFromEnv, textSourceFromRemoteTweetsCSV, textSourceFromTweetsCSV, postPoemWithTable)
 import Web.MarkovBot.MarkovChain (buildTable)
 
 data Options = Options
-  {
-    optionsTweetsCSV :: String
+  { optionsTweetsCSV :: String
+  , optionsOrder :: Int
   } deriving Show
 
 tweetsCSVP :: Parser FilePath
-tweetsCSVP = strOption (
-     long "tweets-csv"
-  <> metavar "FILE"
-  <> help "Path for tweets.csv"
-                          )
+tweetsCSVP = strOption $
+    long "tweets-csv"
+ <> metavar "FILE_OR_URL"
+ <> help "Path or URL for tweets.csv"
+
+orderP :: Parser Int
+orderP = option auto $
+    long "order"
+ <> metavar "INT"
+ <> help "Order of Markov chain"
+ <> showDefault
+ <> value 3
 
 optionsP :: Parser Options
-optionsP = Options <$> tweetsCSVP
+optionsP = Options <$> tweetsCSVP <*> orderP
 
 execute :: Options -> IO ()
 execute opts =
     let tweetsCSV = optionsTweetsCSV opts
+        order = optionsOrder opts
         isURL = "http" `isPrefixOf` tweetsCSV
+        loadTweetsCSV =
+            if isURL
+                then textSourceFromRemoteTweetsCSV
+                else textSourceFromTweetsCSV
      in do
+        unless (order > 0) $ die "order must be greater than 0"
         !twInfo <- getTwInfoFromEnv >>= maybe (putStrLn "environment variables are not set" >> exitFailure) return
-        !table <- bool textSourceFromTweetsCSV textSourceFromRemoteTweetsCSV isURL tweetsCSV >>= buildTablej2
+        !table <- loadTweetsCSV tweetsCSV >>= buildTable order
         postPoemWithTable twInfo table
 
 main :: IO ()
